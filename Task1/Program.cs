02c16f8d24c786fbe60a0c5d11fb58f465c74861
Alexey <alexey.polskiy@gmail.com>
2022-07-14 20:50:05 +0500

    HW10Task1 not good complient

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Есть монотонная последовательность, каждое число встречается ровно то количество раз, 
+// какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... Выведите ряд этой последовательности до N-го значения, желательно использовать рекурсию:
+// N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5"
+
+int Promt(string msg)
+{
+    System.Console.Write(msg);
+    int numberInt = int.Parse(Console.ReadLine());
+    return numberInt;
+}
+
+void Subsequenc(int numberElements, int i = 1)
+{
+    if (numberElements <= 0) return;
+    System.Console.Write($"{i}, ");
+    Subsequenc(numberElements - 1, i + 1);
+    
+}
+
+int numberElements = Promt("Введите число элементов последовательности => ");
+Subsequenc(numberElements);
+
