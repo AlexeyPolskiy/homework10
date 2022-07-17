047da062b148ae6a96d30efb4bbd79f84a664424
Alexey <alexey.polskiy@gmail.com>
2022-07-17 15:03:42 +0500

    homework10 compliet

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,19 +1,33 @@
 ﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 3, n = 2 -> A(m,n) = 29
 
-// Console.Clear(); 
- 
-//  Console.Write("Введите число N: "); 
-//  int countN = Convert.ToInt32(Console.ReadLine()); 
-//  Sequence(countN); 
- 
-//  void Sequence(int count, int number = 0, int countnum = 0){ 
-//     if (count == 0) return; 
-//     for (int i = 0; countnum < count; i++)
-//     { 
-//         countnum = countnum + i; 
-//         number = i; 
-//     } 
-//     Sequence(--count); 
-//     Console.Write(number + " "); 
-//  }
+Console.Clear();
+int Promt(string msg)
+{
+    System.Console.Write(msg);
+    int numberInt = int.Parse(Console.ReadLine());
+    return numberInt;
+}
+
+int Recursion(int numberM, int numberN)
+{
+    // Базовый случай
+    if (numberM == 0)
+    {
+        return numberN + 1;
+    } // Шаг рекурсии / рекурсивное условие
+    else
+    if (numberN == 0 && numberM > 0)
+    {
+        return Recursion(numberM - 1, 1);
+    } // Шаг рекурсии / рекурсивное условие
+    else
+    {
+        return Recursion(numberM - 1, Recursion(numberM, numberN - 1));
+    }
+}
+
+int numberM = Promt("Введите не отрицательное число M => ");
+int numberN = Promt("Введите не отрицательное число N => ");
+
+System.Console.WriteLine(Recursion(numberM, numberN));
